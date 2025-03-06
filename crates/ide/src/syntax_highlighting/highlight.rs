f858087eb8df1a6eb6ba9e9edf7fc9f31e0c188c
GrayJack <gr41.j4ck@gmail.com>
2025-03-06 08:52:30 -0300

    remove `.` as operator

--- crates/ide/src/syntax_highlighting/highlight.rs
+++ crates/ide/src/syntax_highlighting/highlight.rs
@@ -109,7 +109,7 @@ fn punctuation(
             }
             _ => HlOperator::Other.into(),
         },
-        (T![::] | T![->] | T![=>] | T![=] | T![@] | T![.], _) => HlOperator::Other.into(),
+        (T![::] | T![->] | T![=>] | T![=] | T![@], _) => HlOperator::Other.into(),
         (T![!], MACRO_CALL) => {
             if operator_parent
                 .and_then(ast::MacroCall::cast)
